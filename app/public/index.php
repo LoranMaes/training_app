ffc136256ff0ac675be3d01c780169b9e9c4717e
Loran Maes <loran.maes@student.odisee.be>
2023-01-18 22:31:36 +0100

    last fix today

--- app/public/index.php
+++ app/public/index.php
@@ -11,7 +11,34 @@ $router->before('GET|POST', '/.*', function () {
     session_start();
 });
 
-// add your routes and run!
+// To logout a user
+$router->before('GET|POST', '/logout', function () {
+    session_start();
+    // Unset all of the session variables.
+    $_SESSION = [];
+    // If it's desired to kill the session, also delete the session cookie.
+    // Note: This will destroy the session, and not just the session data!
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(
+            session_name(),
+            '',
+            time() - 42000,
+            $params["path"],
+            $params["domain"],
+            $params["secure"],
+            $params["httponly"]
+        );
+    }
+    // Finally, destroy the session.
+    session_destroy();
+
+    // redirect to index
+    header('location: /');
+    exit();
+});
+
+// For the users main content
 $router->before('GET|POST', '/', function () {
     if (!isset($_SESSION['athlete'])) {
         header('Location: /login');
